df69aab7d16bec02d567b7179cf5d375995c0afd
mpeng202 <michael123.peng@gmail.com>
2019-06-30 18:11:04 -0700

    Initial Commit

---
+++ racelace/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  racelace
+//
+//  Created by Michael Peng on 6/30/19.
+//  Copyright © 2019 Michael Peng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
