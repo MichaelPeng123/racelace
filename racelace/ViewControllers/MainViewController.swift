bcdabbcc097eefb9c42f3bd5041a69813dc203e1
mpeng202 <michael123.peng@gmail.com>
2019-07-01 21:17:25 -0700

    tried to implement speed

--- racelace/ViewControllers/MainViewController.swift
+++ racelace/ViewControllers/MainViewController.swift
@@ -8,14 +8,47 @@
 
 import UIKit
 import Firebase
-class ViewController: UIViewController {
+import CoreLocation
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    let locationManager = CLLocationManager()
     @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var speedLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        locationManager.delegate = self
+        if NSString(string:UIDevice.current.systemVersion).doubleValue > 8 {
+            locationManager.requestAlwaysAuthorization()
+        }
+        print("ooooooooooooga")
+        locationManager.desiredAccuracy=kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
+        print("managing")
         
     }
+    
+    //Location managing functions
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        var speed: CLLocationSpeed = CLLocationSpeed()
+        speed = locationManager.location!.speed
+        speedLabel.text = "Speed(m/s): \(speed)"
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
+    
+    private func locationManager(manager: CLLocationManager!, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        if status != CLAuthorizationStatus.denied{
+            print("hi")
+            locationManager.startUpdatingLocation()
+        }
+    }
+    
+    //Timer
     
     var timer = Timer()
     
